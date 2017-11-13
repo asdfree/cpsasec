6fdaf6ac7e75bbf7e77bdffa678846dc49bd6fa0
Anthony Damico <ajdamico@gmail.com>
2017-11-12 20:40:06 -0500

    some bumps

--- tests/setup.R
+++ tests/setup.R
@@ -8,7 +8,7 @@ cpsasec_cat <-
 	get_catalog( "cpsasec" ,
 		output_dir = file.path( getwd() ) )
 
-record_categories <- ceiling( seq( nrow( cpsasec_cat ) ) / ceiling( nrow( cpsasec_cat ) / 10 ) )
+record_categories <- ceiling( seq( nrow( cpsasec_cat ) ) / ceiling( nrow( cpsasec_cat ) / 12 ) )
 
 cpsasec_cat <- unique( rbind( cpsasec_cat[ record_categories == this_sample_break , ] , cpsasec_cat[ cpsasec_cat$year == 2016 , ] ) )
 
