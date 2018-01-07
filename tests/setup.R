fd37550dee75141397f9ad763786a83d1e58d2df
Anthony Damico <ajdamico@gmail.com>
2018-01-07 16:29:33 -0500

    output_dir

--- tests/setup.R
+++ tests/setup.R
@@ -3,7 +3,7 @@ if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 library(lodown)
 lodown( "cpsasec" , output_dir = file.path( getwd() ) )
 this_sample_break <- Sys.getenv( "this_sample_break" )
-cpsasec_cat <- get_catalog( "cpsasec" , cpsasec_cat )
+cpsasec_cat <- get_catalog( "cpsasec" , output_dir = file.path( getwd() ) )
 record_categories <- ceiling( seq( nrow( cpsasec_cat ) ) / ceiling( nrow( cpsasec_cat ) / 12 ) )
 cpsasec_cat <- cpsasec_cat[ record_categories == this_sample_break , ]
 lodown( "cpsasec" , cpsasec_cat )
